16e935ac7c30dfe658dcb573ae3ce494b416ee32
devatcarl-1999 <67814375+devatcarl-1999@users.noreply.github.com>
2020-12-07 10:05:00 -0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Enter a set of numbers");
+            String usrinput = scanner.nextLine().trim();
+            char[] charslt = usrinput.toCharArray();
+            if (isNumber(usrinput)){
+                if (isEvenChar(charslt)) {
+                    System.out.println("All characters are even");
+                } else {
+                    System.out.println("Not all characters are even");
+                }
+            }
+        }
+
+    }
+
+    public static boolean isEvenChar(char[] chars) {
+        int count = 0;
+        int intarr[] = new int[chars.length];
+
+        for (int i = 0; i < chars.length; i++) {
+            intarr[i] = chars[i];
+        }
+		
+        for (int j = 0; j < intarr.length; j++) {
+            if (intarr[j] % 2 != 0){
+                count ++;
+            }
+            if (count > 0){
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+    public static boolean isNumber(String input){
+        long validate = 0;
+        try{
+            validate = Integer.parseInt(input);
+            return true;
+        }catch (Exception e){
+            System.out.println("You must input numbers only!");
+            return false;
+        }
+
+    }
+
+
+}
